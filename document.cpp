4295bf37d7a2d244442575d4aceb89471264e581
Antonio Borondo <antonioborondo@outlook.com>
2018-07-10 12:41:22 +0100

    Code cleanup

--- document.cpp
+++ document.cpp
@@ -27,21 +27,12 @@ document::~document()
 
 const QImage document::get_page_image(unsigned int page_number, unsigned int zoom, float rotate) const
 {
-    fz_pixmap *pix;
-    fz_matrix ctm;
-    int x, y;
-
 //    fz_try(m_context.get())
 //        m_total_pages = fz_count_pages(m_context.get(), m_mupdf_document);
 //    fz_catch(m_context.get())
 //    {
 //        throw std::runtime_error("Pages cannot be counted");
 //    }
-
-    //input = const_cast<char*>(file_path.c_str());
-    page_number = 0;
-    zoom = 100;
-    rotate = 0;
 
 //    if (page_number < 0 || page_number >= m_page_count)
 //    {
@@ -53,10 +44,12 @@ const QImage document::get_page_image(unsigned int page_number, unsigned int zoo
 
     /* Compute a transformation matrix for the zoom and rotation desired. */
     /* The default resolution without scaling is 72 dpi. */
+    fz_matrix ctm;
     fz_scale(&ctm, zoom / 100, zoom / 100);
     fz_pre_rotate(&ctm, rotate);
 
     /* Render page to an RGB pixmap. */
+        fz_pixmap *pix;
     fz_try(m_context.get())
         pix = fz_new_pixmap_from_page_number(m_context.get(), m_mupdf_document, page_number, &ctm, fz_device_rgb(m_context.get()), 0);
     fz_catch(m_context.get())
@@ -67,40 +60,12 @@ const QImage document::get_page_image(unsigned int page_number, unsigned int zoo
         //return EXIT_FAILURE;
     }
 
-    /* Print image data in ascii PPM format. */
-    printf("P3\n");
-    printf("%d %d\n", pix->w, pix->h);
-    printf("255\n");
-    for (y = 0; y < pix->h; ++y)
-    {
-        unsigned char *p = &pix->samples[y * pix->stride];
-        for (x = 0; x < pix->w; ++x)
-        {
-            if (x > 0)
-                printf("  ");
-            printf("%3d %3d %3d", p[0], p[1], p[2]);
-            p += pix->n;
-        }
-        printf("\n");
-    }
+    auto pixmap_samples = fz_pixmap_samples(m_context.get(), pix);
+    auto pixmap_width = fz_pixmap_width(m_context.get(), pix);
+    auto pixmap_height = fz_pixmap_height(m_context.get(), pix);
 
-    unsigned char *samples = NULL;
-    unsigned char *copyed_samples = NULL;
-    int width = 0;
-    int height = 0;
-
-    samples = fz_pixmap_samples(m_context.get(), pix);
-    width = fz_pixmap_width(m_context.get(), pix);
-    height = fz_pixmap_height(m_context.get(), pix);
-
-copyed_samples=samples;
+    //fz_drop_pixmap(m_context.get(), pix);
 
     // with Qt 5.2, Format_RGB888 is correct for any architecture
-            const auto image = QImage(copyed_samples,
-                           width, height, QImage::Format_RGB888, NULL, copyed_samples);
-return image;
-
-    /* Clean up. */
-    fz_drop_pixmap(m_context.get(), pix);
-
+    return QImage(pixmap_samples, pixmap_width, pixmap_height, QImage::Format_RGB888, NULL, pixmap_samples);
 }
