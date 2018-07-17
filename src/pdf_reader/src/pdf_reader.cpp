3c6a158593a67072e2fae757b22faf018d4c2b28
Antonio Borondo <antonioborondo@outlook.com>
2018-07-17 20:38:46 +0100

    Pass smart pointers instead raw pointers to the constructors

--- src/pdf_reader/src/pdf_reader.cpp
+++ src/pdf_reader/src/pdf_reader.cpp
@@ -8,6 +8,7 @@
 
 #include <exception>
 #include <iostream>
+#include <memory>
 
 #include <QLabel>
 #include <QStackedLayout>
@@ -24,16 +25,16 @@ Pdf_reader::Pdf_reader(QWidget* parent)
         const unsigned int zoom = 100;
         const float rotation = 0;
 
-        mupdf_wrapper::Context context;
-        context.register_document_handlers();
+        const auto context = std::make_shared<mupdf_wrapper::Context>();
+        context->register_document_handlers();
 
-        mupdf_wrapper::Document document(&context, filename);
+        const auto document = std::make_shared<mupdf_wrapper::Document>(context, filename);
 
-        mupdf_wrapper::Matrix matrix;
-        matrix.set_zoom(zoom);
-        matrix.set_rotation(rotation);
+        const auto matrix = std::make_shared<mupdf_wrapper::Matrix>();
+        matrix->set_zoom(zoom);
+        matrix->set_rotation(rotation);
 
-        const mupdf_wrapper::Pixmap pixmap(&context, &document, &matrix, page_number);
+        const mupdf_wrapper::Pixmap pixmap(context, document, matrix, page_number);
         const auto samples = pixmap.get_samples();
         const auto width = pixmap.get_width();
         const auto height = pixmap.get_height();
