aa6311817d3eace263ff1b458e1297d191decaf7
Antonio Borondo <antonioborondo@outlook.com>
2018-10-04 14:14:32 +0100

    Add open file menu

--- src/pdf_reader/src/pdf_reader.h
+++ src/pdf_reader/src/pdf_reader.h
@@ -1,7 +1,9 @@
 #ifndef PDF_READER_H
 #define PDF_READER_H
 
+#include <QLabel>
 #include <QMainWindow>
+#include <QStackedLayout>
 
 namespace Ui
 {
@@ -13,10 +15,18 @@ class Pdf_reader : public QMainWindow
     Q_OBJECT
 
     Ui::Pdf_reader* m_ui;
+    QLabel* m_label;
+    QStackedLayout* m_stacked_layout;
 
 public:
-    explicit Pdf_reader(QWidget* parent = 0);
+    explicit Pdf_reader(QWidget* parent = nullptr);
     ~Pdf_reader();
+
+private:
+    void open_file(const std::string& filename);
+
+private slots:
+    void on_menu_open_file_triggered();
 };
 
 #endif
